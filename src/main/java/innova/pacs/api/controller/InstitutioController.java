6182a7147b573b86353e2d02c23c94f4f7e5d9fe
bautistaj <bautistaj20@outlook.com>
2021-08-29 23:25:20 -0500

    (feat) add report

--- src/main/java/innova/pacs/api/controller/InstitutioController.java
+++ src/main/java/innova/pacs/api/controller/InstitutioController.java
@@ -1,8 +1,15 @@
 package innova.pacs.api.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,27 +20,45 @@ import org.springframework.web.bind.annotation.RestController;
 import innova.pacs.api.model.Institution;
 import innova.pacs.api.model.InstitutionUser;
 import innova.pacs.api.model.service.InstitutionService;
+import innova.pacs.api.model.service.ReportService;
 
 @RestController
 @RequestMapping("/api/v1/institutions")
 public class InstitutioController {
 	@Autowired
-	private InstitutionService institutionService; 
-	
+	private InstitutionService institutionService;
+	@Autowired
+	private ReportService reportService;
+
 	@GetMapping()
 	public List<Institution> getAllInstitutions() {
 		return institutionService.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Institution gteById(@PathVariable("id") Integer id) {
 		return institutionService.findById(id);
 	}
-	
-	
-	
+
 	@PostMapping("/configuration")
 	public InstitutionUser getAllInstitutions(@RequestBody InstitutionUser institutionUser) {
 		return institutionService.configure(institutionUser);
+	}
+
+	@GetMapping("/report/{institutionId}")
+	public void generateReportByInctirutionId(@PathVariable Integer institutionId, HttpServletResponse response)
+			throws IOException {
+
+		File file = this.reportService.generateInstitutionReport();
+//			File file = new File("/home/bautistaj/Documentos/dev/innovapacs/reports/example.pdf");
+
+		byte[] byteArrayFile = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+
+		MediaType mediaType = MediaType.APPLICATION_PDF;
+		response.setContentType(mediaType.toString());
+		response.setHeader("Content-Disposition", "inline; filename=intitutionReport.pdf\"");
+		response.setContentLengthLong(byteArrayFile.length);
+		response.getOutputStream().write(byteArrayFile);
+
 	}
 }
---
+++ src/main/java/innova/pacs/api/model/service/ReportService.java
@@ -0,0 +1,163 @@
+package innova.pacs.api.model.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+
+import org.springframework.stereotype.Service;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.pdf.PdfWriter;
+
+@Service
+public class ReportService {
+
+	/**
+	 * Generate report by intitution id
+	 */
+	public File generateInstitutionReport() {
+		try {
+			Document document = new Document(PageSize.LETTER);
+
+			PdfWriter.getInstance(document,
+					new FileOutputStream("/home/bautistaj/Documentos/dev/innovapacs/reports/example.pdf"));
+			
+			document.open();
+			document.addAuthor("Innova Pacs");
+			document.addCreator("Innova Pacs");
+			document.addSubject("Institution Report");
+			document.addCreationDate();
+			document.addTitle("Institution Report");
+
+			HTMLWorker htmlWorker = new HTMLWorker(document);
+			String str = "<!DOCTYPE html>\n"
+					+ "<html lang=\"en\">\n"
+					+ "<head>\n"
+					+ "    <meta charset=\"UTF-8\">\n"
+					+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n"
+					+ "    <style>\n"
+					+ "        body{\n"
+					+ "            width:700px;\n"
+					+ "            margin:0 auto;\n"
+					+ "            background-color:#fff;\n"
+					+ "            border:5px solid #fafafa;\n"
+					+ "            padding:20px;\n"
+					+ "            font-family: Heebo;\n"
+					+ "            font-size: 15px;\n"
+					+ "            font-weight: 300;\n"
+					+ "            text-align: center;\n"
+					+ "            border-collapse: separate;\n"
+					+ "            border-spacing: 0;\n"
+					+ "        }\n"
+					+ "           \n"
+					+ "        table {\n"
+					+ "          color: #67636A;\n"
+					+ "          font-family: Heebo;\n"
+					+ "          font-size: 15px;\n"
+					+ "          font-weight: 300;\n"
+					+ "          text-align: center;\n"
+					+ "          border-collapse: separate;\n"
+					+ "          border-spacing: 0;\n"
+					+ "          width: 99%;\n"
+					+ "          margin: 10px auto;\n"
+					+ "          box-shadow:none;\n"
+					+ "        }\n"
+					+ "        </style>\n"
+					+ "</head>\n"
+					+ "<body style=\"padding: 10px;\">\n"
+					+ "    <table style=\"width: 100%;\">\n"
+					+ "        <tr>\n"
+					+ "            <td>\n"
+					+ "                <img src=\"http://localhost:4200/assets/images/innovapacs.png\" alt=\"\"/>\n"
+					+ "                <img src=\"http://localhost:4200/assets/images/innovapacstext.png\" alt=\"\"/>\n"
+					+ "            </td>\n"
+					+ "            <td><h2>IMAGE Information Systems Ltd</h2></td>\n"
+					+ "        </tr>\n"
+					+ "    </table>\n"
+					+ "    \n"
+					+ "    <table style=\"width: 100%; margin-top: 10px; margin-bottom: 10px;\">\n"
+					+ "        <thead style=\"background-color: #f8f8f8;\">\n"
+					+ "            <tr>\n"
+					+ "                <td><h2 style=\"padding-left: 10px;\">Resumen de estudios</h2></td>\n"
+					+ "                <td><h2 style=\"padding-left: 10px;\">Corte al 29/08/2021</h2></td>\n"
+					+ "            </tr>\n"
+					+ "        </thead>\n"
+					+ "    </table>\n"
+					+ "    \n"
+					+ "\n"
+					+ "    <table style=\"width: 100%;\">\n"
+					+ "        <thead style=\"background-color: #f8f8f8;\">\n"
+					+ "            <tr>\n"
+					+ "                <td> <h3 style=\"padding-left: 10px;\">Pacientes: 1</h3></td>\n"
+					+ "                <td> <h3 style=\"padding-left: 10px;\">Estudios: 1</h3></td>\n"
+					+ "                <td> <h3 style=\"padding-left: 10px;\">Imágenes: 1</h3></td>\n"
+					+ "            </tr>\n"
+					+ "        </thead>\n"
+					+ "    </table>\n"
+					+ "\n"
+					+ "    <h2>Reporte por modalidad</h2>\n"
+					+ "    <table style=\"width: 100%;\">\n"
+					+ "        <thead style=\"background-color: #3498bd;\">\n"
+					+ "            <tr>\n"
+					+ "                <th style=\"font-weight: bold; color: #FFFFFF; padding-top: 5px; padding-bottom: 5px;\">Clave</th>\n"
+					+ "                <th style=\"font-weight: bold; color: #FFFFFF; padding-top: 5px; padding-bottom: 5px;\">Modalidad</th>\n"
+					+ "                <th style=\"font-weight: bold; color: #FFFFFF; padding-top: 5px; padding-bottom: 5px;\">Concepto</th>\n"
+					+ "                <th style=\"font-weight: bold; color: #FFFFFF; padding-top: 5px; padding-bottom: 5px;\">Cantidad</th>\n"
+					+ "            </tr>\n"
+					+ "          </thead>\n"
+					+ "          <tbody style=\"background-color: #f8f8f8;\">\n"
+					+ "                <tr>\n"
+					+ "                  <th scope=\"row\">CT</th>\n"
+					+ "                  <th scope=\"row\">Tomografias</th>\n"
+					+ "                  <th scope=\"row\">Estudios</th>\n"
+					+ "                  <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "                <tr>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\">Pacientes</th>\n"
+					+ "                    <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "                <tr>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\">Imágenes</th>\n"
+					+ "                    <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "                <tr>\n"
+					+ "                    <th scope=\"row\">MR</th>\n"
+					+ "                    <th scope=\"row\">Resononacias</th>\n"
+					+ "                    <th scope=\"row\">Estudios</th>\n"
+					+ "                    <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "                <tr>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\">Pacientes</th>\n"
+					+ "                    <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "                <tr>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\"></th>\n"
+					+ "                    <th scope=\"row\">Imágenes</th>\n"
+					+ "                    <th scope=\"row\">1</th>\n"
+					+ "                </tr>\n"
+					+ "          </tbody>\n"
+					+ "    </table>\n"
+					+ "</body>\n"
+					+ "</html>";
+			htmlWorker.parse(new StringReader(str));
+			document.close();
+
+			System.out.println("Done");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new File("/home/bautistaj/Documentos/dev/innovapacs/reports/example.pdf");
+	}
+
+}
