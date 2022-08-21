9de2d6599fd865d7a6421acb92536aad6d73d918
bautistaj <jcampos@ontop.ai>
2022-08-20 21:43:21 -0500

    (config) cors

--- src/main/java/innova/pacs/api/ApiConfiguration.java
+++ src/main/java/innova/pacs/api/ApiConfiguration.java
@@ -2,19 +2,16 @@ package innova.pacs.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class ApiConfiguration implements WebMvcConfigurer{
-	String methods[] = {"GET", "POST", "PUT", "DELETE", "PATCH"};
-	private String HOST = "http://192.168.3.115:4200";
+@Configuration
+@EnableWebMvc
+public class ApiConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		System.out.println("::::: ApiConfiguration :::::");
-        registry.addMapping("/**")
-        .allowedOrigins(HOST)
-        .allowedMethods(methods);
-		
+		System.out.println(":::: ApiConfiguration ");
+		registry.addMapping("/**");
 	}
 }
