849934b06a08e71922b9a97e0dfa72f06c3bec2a
bautistaj <jcampos@ontop.ai>
2022-11-06 16:57:56 -0600

    Add demo configuration

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Iniciando despliegue"
 #git pull
 
-CID=$(docker ps -aqf "name=innova-pacs-server")
+CID=$(docker ps -aqf "name=innova-pacs-demo-server")
 echo $CID
 
 echo "Detendiendo contenedor"
@@ -11,12 +11,12 @@ echo "Borrando contenedor"
 docker rm $CID
 
 echo "Borrando imagen"
-docker rmi innova-pacs-server:v1
+docker rmi innova-pacs-demo-server:v1
 
 echo "Creando jar de spring"
 mvn clean package -DskipTests -Dmaven.test.skip=true
  
-docker build -t innova-pacs-server:v1 .
-docker run -p 8081:8081 --network dcm4chee_default --name innova-pacs-server -d innova-pacs-server:v1
+docker build -t innova-pacs-demo-server:v1 .
+docker run -p 8081:8081 --network innova_pacs_demo --name innova-pacs-demo-server -d innova-pacs-demo-server:v1
 
 echo "Terminando despliegue"
