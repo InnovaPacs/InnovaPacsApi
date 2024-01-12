f71045522edb60e33f5ef32f4291086a8bb5b0ac
Jose Luis Campos Bautista <bautistaj@Joses-MacBook-Pro.local>
2024-01-12 14:56:53 -0600

    Configiration host

--- src/main/java/innova/pacs/api/ApiConfiguration.java
+++ src/main/java/innova/pacs/api/ApiConfiguration.java
@@ -1,11 +1,13 @@
 package innova.pacs.api;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ApiConfiguration implements WebMvcConfigurer{
 	String methods[] = {"GET", "POST", "PUT", "DELETE", "PATCH"};
-		private String HOST = "http://192.168.3.115:4200";
+		private String HOST = "*";
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
